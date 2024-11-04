b6a3d92670152ad35da5a309683510d16a3d3da2
sanjeevz3009 <sanjeevz1717@gmail.com>
2024-11-04 22:30:38 +0000

    Home Page model added

--- home/models.py
+++ home/models.py
@@ -1,7 +1,15 @@
 from django.db import models
 
 from wagtail.models import Page
+from wagtail.admin.panels import FieldPanel
+from wagtail.fields import RichTextField
 
 
 class HomePage(Page):
-    pass
+    subtitle = models.CharField(max_length=100, blank=True, null=True)
+    body = RichTextField(blank=True)
+
+    content_panels = Page.content_panels + [
+        FieldPanel("subtitle", read_only=True),
+        FieldPanel("body"),
+    ]
